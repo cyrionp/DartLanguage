6591837d7284d10fc188f68beca8fc8c7890f985
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2021-08-12 13:57:28 +0000

    added map

---
+++ map.dart
@@ -0,0 +1,23 @@
+void main(){
+  var dictionary1=new Map();
+  dictionary1["book"]="kitap";
+  dictionary1["little"]="küçük";
+  var dictionary2={"kitap":"book", "küçük":"little"};
+  dictionary2["büyük"]="big";
+
+  print(dictionary1);
+  print(dictionary2);
+
+  dictionary1.remove("book");
+  print(dictionary1);
+
+  for(var value in dictionary2.values){
+    print(value);
+  }
+
+  print(dictionary2.containsKey("kitap"));
+
+  dictionary2.forEach((k,v)=>{
+    print(k+" : "+v)
+  });
+}
